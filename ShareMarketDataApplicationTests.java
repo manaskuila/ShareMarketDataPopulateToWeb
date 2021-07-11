8a6bc774228c6cdc4e016e3c89a08beff2801ece
manaskuila <35089541+manaskuila@users.noreply.github.com>
2021-07-11 08:21:56 +0530

    Add files via upload
    HTML file uploaded.

---
+++ ShareMarketDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manas.org.ShareMarketData;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShareMarketDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
